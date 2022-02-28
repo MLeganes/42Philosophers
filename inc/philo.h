2a6af5310f9f89157e2baaef6cb9d45ae4966757
Angel Morcillo leganes <amorcill@1-D-9.42heilbronn.de>
2022-02-28 17:11:23 +0100

    philo

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 12:05:47 by amorcill          #+#    #+#             */
-/*   Updated: 2022/02/27 17:15:51 by amorcill         ###   ########.fr       */
+/*   Updated: 2022/02/28 16:15:45 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <pthread.h>
 
 /* ************************************************************************** */
 /* COLORS																	..*/
@@ -36,14 +37,39 @@
 /* STRUCT DEFS															  	  */
 /* ************************************************************************** */
 
+typedef enum e_status
+{
+	EATING,
+	SLEEPING,
+	THINKING,
+	DIED,
+} t_status;
+
+
+struct timeval {
+	time_t       tv_sec;   /* seconds since Jan. 1, 1970 */
+	suseconds_t  tv_usec;  /* and microseconds */
+};
+
+struct timezone {
+	int     tz_minuteswest; /* of Greenwich */
+	int     tz_dsttime;     /* type of dst correction to apply */
+};
+
+
+
+
 typedef struct s_philosopher
 {
-	int 	philo;
-	int		ts_takee_fork;
-	int		ts_eating;
-	int		ts_sleeping;
-	int		ts_thinking;
-	int		ts_died;
+	pthread_t	pth_ph;
+	t_status	state;
+	int			ranking_n; //Starting in 1. (index + 1)
+	int			ts_takee_fork;
+	int			ts_eating;
+	int			ts_sleeping;
+	int			ts_thinking;
+	int			ts_died;
+	
 }	t_philosopher;
 
 /***
@@ -52,11 +78,12 @@ typedef struct s_philosopher
 ***/
 typedef struct s_philo
 {
-	int		nphilosophers;
-	int 	time_die;
-	int 	time_eat;
-	int 	time_sleep;
-	int 	ntimes_must_eat;
+	int				nphilosophers;
+	t_philosopher	*philosophers;
+	int				time2die;
+	int				time2eat;
+	int				time2sleep;
+	int				ntimes_must_eat;
 	
 }	t_philo;
 
