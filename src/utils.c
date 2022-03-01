053a2fc5030499333794283c43871352e10002d1
Angel Morcillo leganes <amorcill@1-C-15.42heilbronn.de>
2022-03-01 19:06:24 +0100

    Utils

---
+++ src/utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 15:30:59 by amorcill          #+#    #+#             */
+/*   Updated: 2022/03/01 18:06:23 by amorcill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+
+size_t	ft_strlen(const char *ch)
+{
+	int	count;
+
+	count = 0;
+	if (ch == NULL)
+		return (0);
+	while (*(ch + count))
+	{
+		count++;
+	}
+	return (count);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	return (0);
+}
+
+
+static int	ft_isspace(char c)
+{
+	if ((c == '\t' || c == '\v' || c == '\f' || c == '\r' || c == '\n'
+			|| c == ' '))
+		return (1);
+	return (0);
+}
+
+static int	ft_sign(char c)
+{
+	if (c == '-')
+		return (-1);
+	return (1);
+}
+
+/**
+ * @brief	The ft_strtoi() function converts the string in str to a int value.
+ * 			The string may begin with an arbitrary amount of white space (as 
+ * 			determined by ft_isspace) followed by a single optional `+' or `-' 
+ * 			sign. Base is taken as 10 (decimal) by default.
+ * 
+ * @param str The sting with the number.
+ * @param nbr Pointer to return the integer number.
+ * @return int The ft_strtoi() function return the result of the conversion:
+ * 	1 the conversion was successful.
+ * 	0 is returned, if no conversion could be performed.
+ */
+int	ft_atoi_ext(const char *str, int *nbr)
+{
+	unsigned int	result;
+	int				sign;
+	int				i;
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		sign = ft_sign(str[i]);
+		i++;
+	}
+	while (str[i] != '\0' && result <= 2147483648)
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	if ((sign == -1 && result > 2147483648) || (sign == 1 && result > 2147483647))
+		return (0);
+	*nbr = (int)(sign * result);
+	return (1);
+}
